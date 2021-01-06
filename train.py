9764f0bb0abfd7f7145581863d5279837b0c53c2
Nick Corona <nicklucianocorona@gmail.com>
2021-01-05 20:52:22 -0500

    docstring

--- train.py
+++ train.py
@@ -43,6 +43,7 @@ history = lgb.train(
 
 
 def loguniform(low=0, high=1, size=None, base=10):
+    """Returns a number or a set of numbers from a log uniform distribution"""
     return np.power(base, np.random.uniform(low, high, size))
 
 
